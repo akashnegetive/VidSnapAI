381145a6d7551511963fba3df60d1923bcd347d2
AKASH <78427838+akashnegetive@users.noreply.github.com>
2026-02-03 23:38:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -86,4 +86,6 @@ def gallery():
 
 # -------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=9000)
+    port = int(os.getenv("PORT", 9000))
+    app.run(host="0.0.0.0", port=port)
+
